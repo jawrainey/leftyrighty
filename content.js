3617df199a77a9758b391615ec12e6ab0ae6362e
Jay Rainey <jawrainey@gmail.com>
2014-07-12 22:08:27 +0100

    Refactor repeated checking to a helper method.

--- content.js
+++ content.js
@@ -7,21 +7,32 @@ var next = findHref("next");
 
 /**
  * Find the href for a given name.
- * @param {String} The name of the anchor to search for.
+ * @param {String} name - The name of the anchor to search for.
  * @return {String} The href for a given tag, otherwise an empty string.
  */
 function findHref(name) {
   for (var index = 0; index < links.length; ++index) {
     // The complete anchor HTML element (<a>).
     var anchor = links[index];
-    // Not all anchors have text, rels or classes defined.
-    var rel = (anchor.rel !== undefined) ? anchor.rel : '';
-    var text = (anchor.text !== undefined) ? anchor.text.toLowerCase() : '';
-    var class_name = (anchor.className !== undefined) ? anchor.className : '';
-    if (rel.indexOf(name) > -1 || text.indexOf(name) > -1 || class_name.indexOf(name) > -1) {
-      return anchor.href;
+    // Does the name exist in the anchor?
+    if (isNameInAnchor(name, anchor.rel)  ||
+        isNameInAnchor(name, anchor.text.toLowerCase()) ||
+        isNameInAnchor(name, anchor.className))
+    {
+      return anchor.href
     }
   }
+}
+
+/**
+ * Does the word exist in a given element?
+ * @param {String} name - The name to search for.
+ * @param {String} element - The element to search in.
+ * @return {Boolean} True if the name exists in the element, otherwise false.
+ */
+function isNameInAnchor(name, element) {
+  if (element !== undefined && element.indexOf(name) > -1) return true;
+  else return false
 }
 
 // Go to the next/previous pages using the arrow keys.
