ff2f91c01d6428feaf5cde70bdfa937dd9eb7c6b
Ryan Foster <RytoEX@gmail.com>
2016-04-16 05:23:53 -0400

    Implement most of the inclusion/exclusion features
    This huge commit adds a lot of new code to get the inclusion/exclusion
    features working. The new features are working, though there are still a
    few outstanding to-do entries to make things better.
    
     - Move contents of ./content.js to ./content_script.js and keep
       ./content.js because an extension needs a content script file
    
     - Add ./options.html and ./options.js to handle new user-prefs
    
     - Add ./eventPage.js to actually manage most of the new features
    
     - Update ./manifest.json for permissions and background script

---
+++ eventPage.js
@@ -0,0 +1,192 @@
+/* TODO:
+ *  - allow updates to includeList or excludeList to trigger updateFilters()
+ *    without having to change operationMode first
+ *    check dotheotherthing at (changedKey === "includedDomainsList")
+ *  - maybe set operationMode to "originalMode" by default?
+ */
+
+// add default listeners
+// listen for completed navigation events and Do The Thing
+chrome.webNavigation.onCompleted.addListener(dothething);
+// listen for the Chrome storage to change and Do The Other Thing
+chrome.storage.onChanged.addListener(dotheotherthing);
+
+// check chrome.storage for user prefs
+chrome.storage.sync.get("operationMode", function (items) {
+  // Process the operationMode data
+  console.log("Trying to detect operationMode from chrome.storage");
+  console.log("  operationMode: " + items.operationMode);
+  /* This check is to protect against the case where operationMode is undefined.
+   * This can happen on a fresh install, or if operationMode somehow gets cleared/deleted.
+   */
+  if (items.operationMode) {
+    switchOperationMode(items.operationMode);
+  }
+});
+
+/* define the function with nothing just to appease
+ * chrome.webNavigation.onCompleted.hasListener(_doTheNotThing)
+ */
+var _doTheNotThing = function() {
+}
+
+// inject the main js into the tab from callback
+function dothething(details) {
+  // Zhu Li!  Do the thing!
+  console.log("eventPage.js:  chrome.webNavigation.onCompleted");
+  console.log("  " + details.timeStamp);
+  console.log("  " + details.url);
+  console.log("  we gonna inject the JS here!");
+  chrome.tabs.executeScript(details.tabId, {file: "content_script.js"});
+  /* Do we care about this error?
+   * We could try to test for a chrome:// URL,
+   * or we could just let it silently fail.
+   extensions::lastError:133 Unchecked runtime.lastError while running tabs.executeScript: Cannot access a chrome:// URL
+    at dothething
+   */
+}
+
+// attached to listener on chrome.storage.onChanged
+// mostly used for switching operationMode
+function dotheotherthing(changes, areaName) {
+  // Do the _other_ thing!
+  var changedKey = Object.keys(changes)[0];
+  var newValue = changes[changedKey].newValue;
+  var oldValue = changes[changedKey].oldValue;
+  console.log("Do the other thing! chrome.storage.onChanged: " + areaName);
+  console.log("  changedKeys: " + Object.keys(changes));
+  console.log("  changedKey: " + changedKey);
+  console.log("  newValue: " + newValue);
+  console.log("  oldValue: " + oldValue);
+
+  if (changedKey === "includedDomainsList" || changedKey === "excludedDomainsList" ) {
+    // testing something, double checking that this is an array
+    console.log("  isArray(newValue): " + Array.isArray(newValue));
+    // the below line works if we're updating the urlFilter as we're adding items to the domainList
+    // but we'd have to check if we're in the right operationMode
+    //updateFilters(buildUrlFilters('hostSuffix', newValue));
+  } else if (changedKey === "operationMode") {
+    switchOperationMode(newValue);
+  }
+}
+
+/* called by redefined _doTheNotThing
+ * (attached to listener on chrome.webNavigation.onCompleted)
+ * for excludeList
+ */
+function doTheNotThing(e, urlFilters) {
+  // foreach ExcludeURL: if this URL does not match ALL, do nothing; if this URL does match, set fail flag and break
+  console.log("doTheNotThing!");
+  console.log("  " + e.timeStamp);
+  console.log("  " + e.url);
+  console.log("  urlFilters, array?: " + Array.isArray(urlFilters));
+  console.log("  urlFilters: " + urlFilters);
+
+  /* everything was going so well until this
+   *   Error in event handler for webNavigation.onCompleted: ReferenceError: hasHostSuffix is not defined
+   * It's in the Chrome examples for Filtered events, but apparently doesn't exist.
+   * https://developer.chrome.com/extensions/events#filtered
+   */
+  var fail = false;
+  for (var i = 0; i < urlFilters.length; i++) {
+    if (hasHostSuffix(e.url, urlFilters[i])) {
+      var fail = true;
+      break;
+    }
+  }
+  if (!fail) {
+    console.log("  we gonna inject the JS here!");
+    chrome.tabs.executeScript(e.tabId, {file: "content_script.js"});
+  }
+}
+
+// switch operationMode
+function switchOperationMode(operationMode) {
+  if (operationMode === "originalMode") {
+    // we'll just reset the chrome.webNavigation.onCompleted listener
+    // to not take a urlFilter
+    console.log("Switching to Original Mode");
+    updateFilters();
+  } else if (operationMode === "includeList") {
+    console.log("Switching to Include List");
+    chrome.storage.sync.get({ includedDomainsList: [] }, function (items) {
+      updateFilters(buildUrlFilters('hostSuffix', items.includedDomainsList), "includeList");
+    });
+  } else if (operationMode === "excludeList") {
+    chrome.storage.sync.get({ excludedDomainsList: [] }, function (items) {
+      updateFilters(items.excludedDomainsList, "excludeList");
+    });
+  }
+}
+
+// I might need to refactor something here
+function updateFilters(urlFilters, mode) {
+  if(chrome.webNavigation.onCompleted.hasListener(dothething)) {
+    console.log("removing existing listener (dothething)");
+    chrome.webNavigation.onCompleted.removeListener(dothething);
+  }
+  if(chrome.webNavigation.onCompleted.hasListener(_doTheNotThing)) {
+    console.log("removing existing listener (doTheNotThing)");
+    chrome.webNavigation.onCompleted.removeListener(_doTheNotThing);
+  }
+  if(urlFilters) {
+    console.log("urlFilters got, updating");
+    console.log("  " + urlFilters);
+    if (mode === "includeList") {
+      console.log("adding listener (dothething, {url: urlFilters})");
+      chrome.webNavigation.onCompleted.addListener(dothething, {url: urlFilters});
+    } else if (mode === "excludeList") {
+      console.log("adding listener (doTheNotThing)");
+      // FINALLY! Defining _doTheNotThing as a global var and then redefining as a function works!
+      chrome.webNavigation.onCompleted.addListener(_doTheNotThing = function(details) {
+        doTheNotThing(details, urlFilters);
+      });
+    }
+  } else {
+    console.log("adding listener (dothething)");
+    chrome.webNavigation.onCompleted.addListener(dothething);
+  }
+}
+
+// take an array of URLs and build a url-filter array:
+// https://developer.chrome.com/extensions/events#filtered
+function buildUrlFilters(filterType, urls) {
+  // LOL
+  /* If we accidentally pass a string to the for loop below,
+  it loops over every letter in the string.  XD
+  I might want to test that this is an array...  */
+  var urlFilters = [];
+  for (var i = 0; i < urls.length; i++) {
+    var urlFilter = {};
+    urlFilter[filterType] = urls[i];
+    urlFilters.push(urlFilter);
+  }
+  return urlFilters;
+}
+
+/* Chrome's extension documentation mentions this function
+ * (https://developer.chrome.com/extensions/events#filtered ),
+ * but it doesn't actually exist, so we have to make one.
+ */
+function hasHostSuffix(url, domainSuffix) {
+  var parser = document.createElement('a');
+  parser.href = url;
+
+  // parser.hostname returns full host with subdomains
+  // gonna need to find a way to compare this right
+  // could string search to be lazy?
+  // maybe regex with /(domainSuffix)$/ to ensure end-of-string?
+  // this seems like the way to go:  String.endsWith()
+  console.log(" parser.hostname: " + parser.hostname);
+  console.log(" parser.host: " + parser.host);
+  console.log(" domainSuffix: " + domainSuffix);
+  console.log(" match?: " + parser.hostname.endsWith(domainSuffix));
+  
+  //if (parser.hostname === domainSuffix) {
+  if (parser.hostname.endsWith(domainSuffix)) {
+    console.log(" got a match");
+    return true;
+  } else {
+    return false;
+  }
+}
--- options.js
+++ options.js
@@ -1,37 +1,64 @@
-// Add domain to exclusion list
+// Add domain to inclusion or exclusion list
 function add_domain(evt) {
   evt.preventDefault();
-  var domain = document.getElementById('addDomain').value;
-  var opt = document.createElement('option');
-  opt.text = domain;
-  opt.value = domain;
-  document.getElementById('excludedDomains').add(opt);
+  if (this.id === "excludeForm") {
+    var domain = document.getElementById('excludeAddDomain').value;
+    var opt = document.createElement('option');
+    opt.text = domain;
+    opt.value = domain;
+    document.getElementById('excludedDomains').add(opt);
 
-  chrome.storage.sync.get({
-    excludedDomainsList: []
-  }, function(items) {
-    items.excludedDomainsList.push(domain);
+    chrome.storage.sync.get({
+      excludedDomainsList: []
+    }, function(items) {
+      items.excludedDomainsList.push(domain);
 
-    // Save the array back
-    chrome.storage.sync.set({ excludedDomainsList: items.excludedDomainsList }, function () {
-      var status = document.getElementById('status');
-      if (chrome.runtime.lastError) {
-        status.textContent = 'ERROR: ' + chrome.runtime.lastError.message;
-      } else {
-        // blank out the input field
-        document.getElementById('addDomain').value = "";
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-          status.textContent = '';
-        }, 1300);
-      }
+      // Save the array back
+      chrome.storage.sync.set({ excludedDomainsList: items.excludedDomainsList }, function () {
+        var status_updated = try_update_status('status', 'Options saved', 1300);
+        if (status_updated) {
+          // blank out the input field
+          document.getElementById('excludeAddDomain').value = "";
+        }
+      });
     });
-  });
+  } else if (this.id === "includeForm") {
+    var domain = document.getElementById('includeAddDomain').value;
+    var opt = document.createElement('option');
+    opt.text = domain;
+    opt.value = domain;
+    document.getElementById('includedDomains').add(opt);
+
+    chrome.storage.sync.get({
+      includedDomainsList: []
+    }, function(items) {
+      items.includedDomainsList.push(domain);
+
+      // Save the array back
+      chrome.storage.sync.set({ includedDomainsList: items.includedDomainsList }, function () {
+        var status_updated = try_update_status("status", "Options saved", 1300);
+        if (status_updated) {
+          // blank out the input field
+          document.getElementById('includeAddDomain').value = "";
+        }
+      });
+    });
+  }
 }
 
-// Load the domains from the exclusion list
+// Load the domains from the inclusion and exclusion lists
 function load_domains() {
-  //chrome.storage.sync.clear();
+  chrome.storage.sync.get({ includedDomainsList: [] }, function (items) {
+    // Process the domains
+    var includedDomains = document.getElementById('includedDomains');
+    for (var i = 0; i < items.includedDomainsList.length; i++) {
+      var url = items.includedDomainsList[i];
+      var opt = document.createElement('option');
+      opt.text = url;
+      opt.value = url;
+      includedDomains.add(opt);
+    }
+  });
   chrome.storage.sync.get({ excludedDomainsList: [] }, function (items) {
     // Process the domains
     var excludedDomains = document.getElementById('excludedDomains');
@@ -45,54 +72,145 @@ function load_domains() {
   });
 }
 
-// Remove selected domains from the exclusion list
+// Remove selected domains from the inclusion or exclusion list
 function remove_selected_domains() {
-  var excludedDomains = document.getElementById('excludedDomains');
-  var excludedDomainsRemoveList = [];
-  for (var i = excludedDomains.length - 1; i >= 0; i--) {
-    if (excludedDomains.options[i].selected) {
-      excludedDomainsRemoveList.push(excludedDomains.options[i].text);
-      excludedDomains.remove(i);
-    }
-  }
-
-  chrome.storage.sync.get({
-    excludedDomainsList: []
-  }, function(items) {
-    var excludedDomainsRemoveIndexes = [];
-    for (var i = 0; i < excludedDomainsRemoveList.length; i++) {
-      // splice out domains marked for removal
-      items.excludedDomainsList.splice(items.excludedDomainsList.indexOf(excludedDomainsRemoveList[i]), 1);
-    }
-
-    // Save the array back
-    chrome.storage.sync.set({ excludedDomainsList: items.excludedDomainsList }, function () {
-      var status = document.getElementById('status');
-      if (chrome.runtime.lastError) {
-        status.textContent = 'ERROR: ' + chrome.runtime.lastError.message;
-      } else {
-        status.textContent = 'Domain removed.';
-        setTimeout(function() {
-          status.textContent = '';
-        }, 1300);
+  if (this.id === "excludeRemove") {
+    var excludedDomains = document.getElementById('excludedDomains');
+    var excludedDomainsRemoveList = [];
+    for (var i = excludedDomains.length - 1; i >= 0; i--) {
+      if (excludedDomains.options[i].selected) {
+        excludedDomainsRemoveList.push(excludedDomains.options[i].text);
+        excludedDomains.remove(i);
       }
+    }
+
+    chrome.storage.sync.get({
+      excludedDomainsList: []
+    }, function(items) {
+      var excludedDomainsRemoveIndexes = [];
+      for (var i = 0; i < excludedDomainsRemoveList.length; i++) {
+        // splice out domains marked for removal
+        items.excludedDomainsList.splice(items.excludedDomainsList.indexOf(excludedDomainsRemoveList[i]), 1);
+      }
+
+      // Save the array back
+      chrome.storage.sync.set({ excludedDomainsList: items.excludedDomainsList }, function () {
+        try_update_status("status", "Selected domains removed", 1300);
+      });
     });
-  });
+  } else if (this.id === "includeRemove") {
+    var includedDomains = document.getElementById('includedDomains');
+    var includedDomainsRemoveList = [];
+    for (var i = includedDomains.length - 1; i >= 0; i--) {
+      if (includedDomains.options[i].selected) {
+        includedDomainsRemoveList.push(includedDomains.options[i].text);
+        includedDomains.remove(i);
+      }
+    }
+
+    chrome.storage.sync.get({
+      includedDomainsList: []
+    }, function(items) {
+      var includedDomainsRemoveIndexes = [];
+      for (var i = 0; i < includedDomainsRemoveList.length; i++) {
+        // splice out domains marked for removal
+        items.includedDomainsList.splice(items.includedDomainsList.indexOf(includedDomainsRemoveList[i]), 1);
+      }
+
+      // Save the array back
+      chrome.storage.sync.set({ includedDomainsList: items.includedDomainsList }, function () {
+        try_update_status("status", "Selected domains removed", 1300);
+      });
+    });
+  }
 }
 
-// Clear all options
+// Clear all saved option data
 function clear_all_data() {
   chrome.storage.sync.clear();
 }
 
-document.addEventListener('DOMContentLoaded', load_domains);
-/*document.getElementById('add').addEventListener('click',
-    add_domain);*/
-document.getElementById('excludeForm').addEventListener('submit',
-    add_domain);
-document.getElementById('load').addEventListener('click',
-    load_domains);
-document.getElementById('remove').addEventListener('click',
-    remove_selected_domains);
-document.getElementById('clear_all_data').addEventListener('click',
-    clear_all_data);
+// wrapper for "update_status" to reduce code duplication with checking chrome.runtime.lastError
+function try_update_status(eId, msg, timeout) {
+  var success = true;
+  if (chrome.runtime.lastError) {
+    msg = 'ERROR: ' + chrome.runtime.lastError.message;
+    timeout = -1;
+    success = false;
+  }
+  update_status(eId, msg, timeout);
+  return success;
+}
+
+// Update status element with a status message
+function update_status(eId, msg, timeout) {
+  var elStatus = document.getElementById(eId);
+  elStatus.textContent = msg;
+  if (Number.isInteger(timeout) && timeout >= 0) {
+    setTimeout(function() {
+      elStatus.textContent = '';
+    }, timeout);
+  }
+}
+
+// swap visibility of option tabs
+function toggle_tabs() {
+  // get currently active tab that we're navigating away from
+  var activeTab = document.getElementById("tabs").getElementsByClassName("activeTab")[0];
+
+  // Swap the active tab
+  this.classList.add("activeTab");
+  activeTab.classList.remove("activeTab");
+  // Swap the active tab-contents
+  document.getElementById(this.id + "Content").classList.remove("hidden");
+  document.getElementById(activeTab.id + "Content").classList.add("hidden");
+  // Toggle the eventListeners
+  this.removeEventListener("click", toggle_tabs);
+  activeTab.addEventListener("click", toggle_tabs);
+}
+
+// Save the main/general options
+function save_main_options() {
+  chrome.storage.sync.set({ operationMode: this.value}, function (items) {
+    try_update_status('status', 'Options saved', 1300);
+  });
+}
+
+// Load the main/general options
+function load_main_options() {
+  chrome.storage.sync.get( "operationMode" , function (items) {
+    // Process the operationMode data
+    var radioGroupOperationModes = document.getElementsByName('operationMode');
+    for (var i = 0; i < radioGroupOperationModes.length; i++) {
+      if (radioGroupOperationModes[i].value === items.operationMode) {
+        radioGroupOperationModes[i].checked = true;
+      } else {
+        radioGroupOperationModes[i].checked = false;
+      }
+    }
+  });
+}
+
+// load all user saved preferences
+function load_all_preferences() {
+  // load the domains
+  load_domains();
+  // load the main options
+  load_main_options();
+}
+
+
+document.addEventListener('DOMContentLoaded', load_all_preferences);
+// we don't need to add the listener for the tab that is open by default
+//document.getElementById('includeTab').addEventListener('click', toggle_tabs);
+document.getElementById('excludeTab').addEventListener('click', toggle_tabs);
+document.getElementById('mainOptionsTab').addEventListener('click', toggle_tabs);
+document.getElementById('includeForm').addEventListener('submit', add_domain);
+document.getElementById('includeRemove').addEventListener('click', remove_selected_domains);
+document.getElementById('excludeForm').addEventListener('submit', add_domain);
+document.getElementById('excludeRemove').addEventListener('click', remove_selected_domains);
+document.getElementById('load').addEventListener('click', load_all_preferences);
+document.getElementById('clear_all_data').addEventListener('click', clear_all_data);
+document.getElementsByName('operationMode')[0].addEventListener('click', save_main_options);
+document.getElementsByName('operationMode')[1].addEventListener('click', save_main_options);
+document.getElementsByName('operationMode')[2].addEventListener('click', save_main_options);
